b710c7177b483bca166fab69d1b9637849ab39b2
Kubrashaik <kubrashaik32@gmail.com>
2021-10-08 19:47:36 +0530

    updated

--- gitEx/src/gitEx/printHello.java
+++ gitEx/src/gitEx/printHello.java
@@ -3,5 +3,6 @@ package gitEx;
 public class printHello {
          public static void main(String[] args) {
         	 System.out.println("Hello World");
+                 System.out.println("Hi");
          }
 }
